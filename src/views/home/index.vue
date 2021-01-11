305967891f3257af3a07a1ce3d26cefdaa492566
reinfajar <tsanimfajar@outlook.com>
2021-01-11 15:40:33 +0700

    change home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,17 +10,15 @@
         </p>
         <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
           <div class="flex relative pb-12">
-            <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-              <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-            </div>
             <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
             </div>
             <div class="flex-grow pl-4">
-              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Elementary School</h2>
-              <p class="leading-relaxed">SD Islam Zakaria 2001-2007</p>
+              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
+              <p class="leading-relaxed">Hacktiv8 - Fullstack Javascript Bootcamp Nov 2019 - May 2020</p>
             </div>
           </div>
           <div class="flex relative pb-12">
@@ -34,8 +32,8 @@
               </svg>
             </div>
             <div class="flex-grow pl-4">
-              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Middle School</h2>
-              <p class="leading-relaxed">SMP Istiqamah 2007-2010</p>
+              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">College</h2>
+              <p class="leading-relaxed">Chemistry, Faculty of Mathematics and Natural Science, Padjajaran University, 2013-2018</p>
             </div>
           </div>
           <div class="flex relative pb-12">
@@ -64,20 +62,22 @@
               </svg>
             </div>
             <div class="flex-grow pl-4">
-              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">College</h2>
-              <p class="leading-relaxed">Chemistry, Faculty of Mathematics and Natural Science, Padjajaran University, 2013-2018</p>
+              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Middle School</h2>
+              <p class="leading-relaxed">SMP Istiqamah 2007-2010</p>
             </div>
           </div>
           <div class="flex relative">
+            <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+              <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
             <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                <path d="M22 4L12 14.01l-3-3"></path>
+                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
               </svg>
             </div>
             <div class="flex-grow pl-4">
-              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
-              <p class="leading-relaxed">Hacktiv8 - Fullstack Javascript Bootcamp Nov 2019 - May 2020</p>
+              <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Elementary School</h2>
+              <p class="leading-relaxed">SD Islam Zakaria 2001-2007</p>
             </div>
           </div>
         </div>
